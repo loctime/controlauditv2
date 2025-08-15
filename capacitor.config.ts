306244e105665c61618f60a07cdf6ba4a3dfadd9
Diego Bertosi <diegobertosi@gmail.com>
2025-08-15 17:37:30 -0300

    apk

---
+++ capacitor.config.ts
@@ -0,0 +1,23 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.controlaudit.app',
+  appName: 'Control de Auditorías',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      backgroundColor: "#1976d2",
+      showSpinner: true,
+      spinnerColor: "#ffffff"
+    },
+    StatusBar: {
+      style: 'dark'
+    }
+  }
+};
+
+export default config;
