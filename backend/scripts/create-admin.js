c937188c591e7f331d5c32d9fe470f49293f0d99
Diego Bertosi <diegobertosi@gmail.com>
2026-02-18 13:02:42 -0300

    viendo que onda

--- backend/scripts/create-admin.js
+++ backend/scripts/create-admin.js
@@ -12,7 +12,7 @@ import admin from '../firebaseAdmin.js';
 // ===============================
 // CONFIGURACIÓN OWNER
 // ===============================
-const EMAIL = 'hys@maximia.com.ar';
+const EMAIL = 'carlascazziotta@hotmail.com';
 const PASSWORD = '123123123';
 const DISPLAY_NAME = 'Owner Principal';
 
